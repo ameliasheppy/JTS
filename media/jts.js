4a0392e7e800c30c7a92228f86bb09f674a42485
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2022-07-21 00:52:34 -0400

    Learning experience!
    This evening I decided to spend it doing something I truly enjoy. CODING! Looking things up on MDN, and just making a fun, functional site that is what I envisioned. Done with the index! Had fun, learned several new things. Such as setting a video as a background, making a menu with images and text on it to select.

---
+++ media/jts.js
@@ -0,0 +1,16 @@
+.back-video{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+}
+@media(max-aspect-ratio:16/9){
+    .back-video{
+        width: 100%;
+        height: auto;
+    }
+}
